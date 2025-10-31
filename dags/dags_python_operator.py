f4af3758324e9563de7b1cc720806b2e935c6856
jhkim <j.h.kim6844@gmail.com>
2025-10-31 09:26:32 +0900

    python op dags 업로드

---
+++ dags/dags_python_operator.py
@@ -0,0 +1,25 @@
+import pendulum
+import random
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    def select_fruit():
+        fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+        rand_int = random.randint(0, 4)
+        selected_fruit = random.choice(fruits)
+        print(fruits[rand_int])
+
+    py_t1 = PythonOperator(
+        task_id='py_t1',
+        python_callable=select_fruit
+    )
+
+py_t1

7beb6002f1995b80a0be7c644d8eff28938d046f
jhkim <j.h.kim6844@gmail.com>
2025-11-05 07:15:29 +0900

    task_decorator 업로드

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -1,18 +1,23 @@
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime
 import pendulum
-from airflow.sdk import DAG, task
+import logging
 
+# DAG 기본 설정
 with DAG(
-    dag_id="dags_python_task_decorator",
-    schedule="0 2 * * 1",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    dag_id='dags_python_task_decorator',
+    schedule='@daily',
+    start_date=pendulum.datetime(2023, 3, 1, tz='Asia/Seoul'),
     catchup=False,
+    tags=['example']
 ) as dag:
     
     @task(task_id="python_task1")
-    def print_context(some_input):
+    def print_context(some_input: str):
+        """입력값을 로그와 print로 출력"""
         print(some_input)
+        logging.info(some_input)
 
+    # 태스크 정의 (Airflow 3.0에서도 동일)
     python_task1 = print_context('task_decorator_실행')
-
-
-        

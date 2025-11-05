af677b29ba8461c78b322e36faf029a81affb02b
jhkim <j.h.kim6844@gmail.com>
2025-11-05 16:44:04 +0900

    op_args 테스용 op 업로드

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from datetime import datetime
+import pendulum
+from plugins.common.common_func import regist
+
+# DAG 기본 설정
+with DAG(
+    dag_id='dags_python_with_op_args',
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    # PythonOperator를 사용하여 태스크 정의
+    regist_t1 = PythonOperator(
+        task_id='regist_t1',
+        python_callable=regist,
+        op_args=['홍길동', '남자', 'kr', '서울']
+    )
+
+    regist_t1
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,7 @@
 def get_sftp():
     print('SFTP function called')
+
+def regist(name,gender,*arg):
+    print(f'이름: {name}, 성별: {gender}')
+    print(f'기타옵션들: {arg}')    
+    

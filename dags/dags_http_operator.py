3c0624e53848e3b6215349d7a5ae58c70d685105
jhkim <j.h.kim6844@gmail.com>
2025-11-17 21:19:10 +0900

    dags http operator 업로드

---
+++ dags/dags_http_operator.py
@@ -0,0 +1,35 @@
+import pendulum
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow import DAG
+
+with DAG(
+    dag_id="dags_http_operator",
+    schedule="0 9 * * *",
+    start_date=pendulum.datetime(2023, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    schedule = None 
+) as dag:
+    
+    ##서울시 공공데이터
+    http_task = SimpleHttpOperator(
+        task_id='tb_seoul_jobinfo',
+        method='GET',
+        http_conn_id='openapi.seoul.go.kr',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/JSON/GetJobInfo/1/5/',
+        method ='GET',
+        headers= {'Content-Type': 'application/json',
+                        'charset': 'utf-8',
+                        'Accept': '*/*'
+                        }
+    )
+
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='tb_seoul_jobinfo')
+        import json
+        from pprint import pprint
+        json_rslt = json.loads(rslt)
+        pprint(json_rslt)
+
+    http_task >> python_2()
